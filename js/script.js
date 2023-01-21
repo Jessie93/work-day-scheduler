f9913794e38300bc355f7e934f19031c082ca7ea
jessica stephenson <jesslouisestephenson@gmail.com>
2023-01-21 15:06:39 +0000

    added more code to the js

--- js/script.js
+++ js/script.js
@@ -10,3 +10,39 @@ $("#currentDay").text(moment().format('dddd Do MMMM'));
 var currentTime = moment().format('HH:mm:ss');
 console.log(currentTime);
 
+// Function to add colours to the calendar 
+function colour() {
+    // this code gets the current hour using moment
+    var hour = moment().hours();
+// function for all Element with the class time-block
+    $(".time-block").each(function() {
+        var currentHour = parseInt($(this).attr("id"));
+//  code below checks if the time is in the future, present or past and gives it a colour in the CSS. Ive used a switch statement as i like them more but you could use an If statement. ive also had to ass come class's to make this section of code to work
+        switch (true) {
+            case currentHour > hour:
+                $(this).addClass("future");
+                break;
+            case currentHour === hour:
+                $(this).addClass("present");
+                break;
+            default:
+                $(this).addClass("past");
+                break;
+        }
+    });
+};
+
+// Function for the save button
+function saveTask() {
+    // added a click event listener. when the button is clicked the following happens
+    $(".saveBtn").on("click", function() {
+        // get the text of the hour element and the value of the task input
+        var time = $(this).siblings(".hour").text();
+        var task = $(this).siblings(".task").val();
+
+        // save the calendar task to local storage
+        localStorage.setItem(time, task);
+    });
+}
+
+
